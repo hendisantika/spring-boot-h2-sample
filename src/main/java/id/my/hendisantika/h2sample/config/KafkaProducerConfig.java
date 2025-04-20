ba8014db3140c15a58c61a4385fae7a0cd1f1f36
hendisantika <hendisantika@yahoo.co.id>
2025-04-20 21:18:33 +0700

    feat: Add producerFactory in KafkaProducerConfig🫸🌀✏️📗🐧🐳

--- src/main/java/id/my/hendisantika/h2sample/config/KafkaProducerConfig.java
+++ src/main/java/id/my/hendisantika/h2sample/config/KafkaProducerConfig.java
@@ -1,7 +1,16 @@
 package id.my.hendisantika.h2sample.config;
 
+import com.fasterxml.jackson.databind.ser.std.StringSerializer;
+import id.my.hendisantika.h2sample.entity.Student;
+import org.apache.kafka.clients.producer.ProducerConfig;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.core.DefaultKafkaProducerFactory;
+import org.springframework.kafka.core.ProducerFactory;
+
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * Created by IntelliJ IDEA.
@@ -19,4 +28,20 @@ public class KafkaProducerConfig {
 
     @Value(value = "${spring.kafka.bootstrap-servers}")
     private String bootstrapAddress;
+
+    @Bean
+    public ProducerFactory<String, Student> producerFactory() {
+        Map<String, Object> configProps = new HashMap<>();
+        configProps.put(
+                ProducerConfig.BOOTSTRAP_SERVERS_CONFIG,
+                bootstrapAddress);
+        configProps.put(
+                ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG,
+                StringSerializer.class);
+        configProps.put(
+                ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG,
+                CustomStudentSerializer.class);
+        return new DefaultKafkaProducerFactory<>(configProps);
+    }
+
 }
