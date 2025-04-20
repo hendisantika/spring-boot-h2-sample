367446648c005f6cd295fd7a198ebbdf247e3f32
hendisantika <hendisantika@yahoo.co.id>
2025-04-20 21:19:53 +0700

    feat: Add topic in KafkaTopicConfig🫸🌀✏️📗🐧🐳

--- src/main/java/id/my/hendisantika/h2sample/config/KafkaTopicConfig.java
+++ src/main/java/id/my/hendisantika/h2sample/config/KafkaTopicConfig.java
@@ -1,6 +1,7 @@
 package id.my.hendisantika.h2sample.config;
 
 import org.apache.kafka.clients.admin.AdminClientConfig;
+import org.apache.kafka.clients.admin.NewTopic;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -31,5 +32,10 @@ public class KafkaTopicConfig {
         Map<String, Object> configs = new HashMap<>();
         configs.put(AdminClientConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapAddress);
         return new KafkaAdmin(configs);
+    }
+
+    @Bean
+    public NewTopic topic() {
+        return new NewTopic("student_topic", 1, (short) 1);
     }
 }
