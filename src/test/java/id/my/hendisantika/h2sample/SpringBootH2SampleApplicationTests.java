106b3e6a7972ed54eb35a155099efd5102651294
hendisantika <hendisantika@yahoo.co.id>
2025-04-20 21:09:36 +0700

    feat: Init Project🫸🌀✏️📗🐧🐳

---
+++ src/test/java/id/my/hendisantika/h2sample/SpringBootH2SampleApplicationTests.java
@@ -0,0 +1,13 @@
+package id.my.hendisantika.h2sample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootH2SampleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
