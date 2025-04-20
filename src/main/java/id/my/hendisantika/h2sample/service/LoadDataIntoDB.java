d495e6a9e228f69a2039dc12f388d4a2d1931d29
hendisantika <hendisantika@yahoo.co.id>
2025-04-20 21:15:29 +0700

    feat: Add LoadDataIntoDB🫸🌀✏️📗🐧🐳

---
+++ src/main/java/id/my/hendisantika/h2sample/service/LoadDataIntoDB.java
@@ -0,0 +1,26 @@
+package id.my.hendisantika.h2sample.service;
+
+import id.my.hendisantika.h2sample.repository.StudentRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-boot-h2-sample
+ * User: hendisantika
+ * Link: s.id/hendisantika
+ * Email: hendisantika@yahoo.co.id
+ * Telegram : @hendisantika34
+ * Date: 20/04/25
+ * Time: 21.15
+ * To change this template use File | Settings | File Templates.
+ */
+@Service
+@RequiredArgsConstructor
+public class LoadDataIntoDB {
+
+    private final StudentRepository studentRepository;
+
+    private final int batch = 1000;
+    private final int start = 0;
+}
