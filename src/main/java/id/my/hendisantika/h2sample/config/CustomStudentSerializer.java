1d61abd24d79fc1f29a9c7192edb1e6914ed03aa
hendisantika <hendisantika@yahoo.co.id>
2025-04-20 21:17:43 +0700

    feat: Add CustomStudentSerializer🫸🌀✏️📗🐧🐳

---
+++ src/main/java/id/my/hendisantika/h2sample/config/CustomStudentSerializer.java
@@ -0,0 +1,44 @@
+package id.my.hendisantika.h2sample.config;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import id.my.hendisantika.h2sample.entity.Student;
+import org.apache.kafka.common.errors.SerializationException;
+import org.apache.kafka.common.serialization.Serializer;
+
+import java.util.Map;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-boot-h2-sample
+ * User: hendisantika
+ * Link: s.id/hendisantika
+ * Email: hendisantika@yahoo.co.id
+ * Telegram : @hendisantika34
+ * Date: 20/04/25
+ * Time: 21.17
+ * To change this template use File | Settings | File Templates.
+ */
+public class CustomStudentSerializer implements Serializer<Student> {
+    private final ObjectMapper objectMapper = new ObjectMapper();
+
+    @Override
+    public void configure(Map<String, ?> configs, boolean isKey) {
+    }
+
+    @Override
+    public byte[] serialize(String topic, Student data) {
+        try {
+            if (data == null) {
+                System.out.println("Null received at serializing");
+                return null;
+            }
+            return objectMapper.writeValueAsBytes(data);
+        } catch (Exception e) {
+            throw new SerializationException("Error when serializing MessageDto to byte[]");
+        }
+    }
+
+    @Override
+    public void close() {
+    }
+}
