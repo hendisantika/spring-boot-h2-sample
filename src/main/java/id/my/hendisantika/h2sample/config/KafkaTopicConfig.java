9b5d0e334cd396c6a846d7c16ca95d1cc9c199d7
hendisantika <hendisantika@yahoo.co.id>
2025-04-20 21:19:37 +0700

    feat: Add kafkaAdmin in KafkaTopicConfig🫸🌀✏️📗🐧🐳

--- src/main/java/id/my/hendisantika/h2sample/config/KafkaTopicConfig.java
+++ src/main/java/id/my/hendisantika/h2sample/config/KafkaTopicConfig.java
@@ -1,7 +1,13 @@
 package id.my.hendisantika.h2sample.config;
 
+import org.apache.kafka.clients.admin.AdminClientConfig;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.core.KafkaAdmin;
+
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * Created by IntelliJ IDEA.
@@ -19,4 +25,11 @@ public class KafkaTopicConfig {
 
     @Value(value = "${spring.kafka.bootstrap-servers}")
     private String bootstrapAddress;
+
+    @Bean
+    public KafkaAdmin kafkaAdmin() {
+        Map<String, Object> configs = new HashMap<>();
+        configs.put(AdminClientConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapAddress);
+        return new KafkaAdmin(configs);
+    }
 }
